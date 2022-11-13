80ea95977dfecae160fa9d63254ab7bd7176797c
SivaSurender <e.surender14@gmail.com>
2022-11-13 15:19:06 +0530

    Initial fetch req

--- dist/index.e37f48ea.js
+++ dist/index.e37f48ea.js
@@ -539,8 +539,33 @@ const timeout = function(s) {
             reject(new Error(`Request took too long! Timeout after ${s} second`));
         }, s * 1000);
     });
-}; // https://forkify-api.herokuapp.com/v2
- ///////////////////////////////////////
+};
+// https://forkify-api.herokuapp.com/v2
+///////////////////////////////////////
+const showRecipe = async function() {
+    try {
+        const response = await fetch("https://forkify-api.herokuapp.com/api/v2/recipes/5ed6604591c37cdc054bc886");
+        const finalResponse = await response.json();
+        // error handle for invalid ID
+        if (!response.ok) throw new Error(`${finalResponse.message} (${response.status})`);
+        // destructuring recipe to readable terms
+        let { recipe  } = finalResponse.data;
+        recipe = {
+            id: recipe.id,
+            imageURL: recipe.image_url,
+            ingredients: recipe.ingredients,
+            publisher: recipe.publisher,
+            servings: recipe.servings,
+            sourceURL: recipe.source_url,
+            title: recipe.title,
+            cookingTime: recipe.cooking_time
+        };
+        console.log(recipe);
+    } catch (err) {
+        alert(err);
+    }
+};
+showRecipe();
 
 },{}]},["fA0o9","aenu9"], "aenu9", "parcelRequire7083")
 
--- src/js/controller.js
+++ src/js/controller.js
@@ -11,3 +11,35 @@ const timeout = function (s) {
 // https://forkify-api.herokuapp.com/v2
 
 ///////////////////////////////////////
+
+const showRecipe = async function () {
+  try {
+    const response = await fetch(
+      'https://forkify-api.herokuapp.com/api/v2/recipes/5ed6604591c37cdc054bc886'
+    );
+
+    const finalResponse = await response.json();
+
+    // error handle for invalid ID
+    if (!response.ok)
+      throw new Error(`${finalResponse.message} (${response.status})`);
+    // destructuring recipe to readable terms
+
+    let { recipe } = finalResponse.data;
+    recipe = {
+      id: recipe.id,
+      imageURL: recipe.image_url,
+      ingredients: recipe.ingredients,
+      publisher: recipe.publisher,
+      servings: recipe.servings,
+      sourceURL: recipe.source_url,
+      title: recipe.title,
+      cookingTime: recipe.cooking_time,
+    };
+    console.log(recipe);
+  } catch (err) {
+    alert(err);
+  }
+};
+
+showRecipe();
